8161cd628bad52a0e270bcd6e322d2374168f228
Lourenço Passos <lo.passos93@gmail.com>
2020-09-24 16:31:55 -0300

    deploy v6

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import {AddressInfo} from "net";
+import { AddressInfo } from "net";
 import express from "express";
 import { userRouter } from "./routes/userRouter";
 import { photoRouter } from "./routes/photoRouter";
@@ -9,13 +9,13 @@ const app = express();
 app.use(express.json());
 
 app.use("/user", userRouter);
-app.use("/image", photoRouter)
+app.use("/image", photoRouter);
 
-const server = app.listen(8000, () => {
-    if (server) {
-      const address = server.address() as AddressInfo;
-      console.log(`Servidor rodando em http://localhost:${address.port}`);
-    } else {
-      console.error(`Falha ao rodar o servidor.`);
-    }
-  });
+const server = app.listen(process.env.PORT || 3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Servidor rodando em http://localhost:${address.port}`);
+  } else {
+    console.error(`Falha ao rodar o servidor.`);
+  }
+});
